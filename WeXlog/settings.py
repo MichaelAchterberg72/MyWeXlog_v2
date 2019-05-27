fe92d720665a93c0da1694f31b04dcefebe02edf
jkeir <jcm.keir@gmail.com>
2019-05-27 21:19:05 +0200

    Added HTML header safety 3rd party apps to MIDDLEWARE

--- WeXlog/settings.py
+++ WeXlog/settings.py
@@ -64,6 +64,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+            #3rd party MIDDLEWARE
+    'django_referrer_policy.middleware.ReferrerPolicyMiddleware',
+    'csp.middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'WeXlog.urls'
