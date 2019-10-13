44be6444000c658a2579a2fbbdb2eb804807d10f
JamesTKGDB <jcm.keir@gmail.com>
2019-10-13 17:29:59 +0200

    move allauth templates to local templates

--- public/models.py
+++ public/models.py
@@ -7,21 +7,22 @@ from django_countries.fields import CountryField
 
 
 class ContactUs(models.Model):
-    date = models.DateTimeField(null=False, blank=False, default=timezone.now())
+    date = models.DateTimeField(null=False, blank=False, default=timezone.now)
     name = models.CharField(max_length=60, null=True, blank=True)
     email = models.EmailField(max_length=254, blank=True)
     comments = models.TextField(null=True, blank=True)
 
 
 class Suggestions(models.Model):
-    date = models.DateTimeField(null=False, blank=False, default=datetime.now())
+    date = models.DateTimeField(null=False, blank=False, default=timezone.now)
     name = models.CharField(max_length=60, null=True, blank=True)
     email = models.EmailField(max_length=254, blank=True)
     comments = models.TextField(null=True, blank=True)
 
 
 class DataProtection(models.Model):
-    date = models.DateTimeField(null=False, blank=False, default=datetime.now())
+    date = models.DateTimeField(null=False, blank=False, default=timezone.now)
     name = models.CharField(max_length=60, null=True, blank=True)
     email = models.EmailField(max_length=254, blank=True)
     comments = models.TextField(null=True, blank=True)
+#
