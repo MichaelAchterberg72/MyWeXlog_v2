ef82d534cc5dbfcff85f4793c90294c790209b91
MichaelAchterberg <machterberg@devoptec.com>
2020-05-24 20:34:10 +0200

    Invites Update

--- users/forms.py
+++ users/forms.py
@@ -57,15 +57,15 @@ class CustomSignupForm(SignupForm):
 #        else:
 #            raise forms.ValidationError("Your are required to be sixteen years old or older!")
 #
-#    def save(self, request):
-#
-#        CustomUser = super(CustomSignupForm, self).save(request)
-#        CustomUser.first_name=self.cleaned_data['first_name']
-#        CustomUser.last_name=self.cleaned_data['last_name']
-#        CustomUser.terms=self.cleaned_data['terms']
-#        CustomUser.age_accept=self.cleaned_data['age_accept']
-#
-#        return CustomUser
+    def save(self, request):
+
+        CustomUser = super(CustomSignupForm, self).save(request)
+        CustomUser.first_name=self.cleaned_data['first_name']
+        CustomUser.last_name=self.cleaned_data['last_name']
+        CustomUser.terms=self.cleaned_data['terms']
+        CustomUser.age_accept=self.cleaned_data['age_accept']
+
+        return CustomUser
 
 
 class CustomUserSettingsForm(forms.ModelForm):
