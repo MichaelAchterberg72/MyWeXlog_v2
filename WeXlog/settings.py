84ec229cba966465245c1d52e26451fc4cd00b1b
JamesTKGDB <jcm.keir@gmail.com>
2020-09-01 20:01:47 +0200

    JIRA MYW-241

--- WeXlog/settings.py
+++ WeXlog/settings.py
@@ -333,7 +333,23 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'prod_static')
 
 MEDIA_URL = '/library/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'filelibrary')
+'''
+#files for Amazon S3
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'WeXlog.storage_backends.MediaStorage'
+'''
 #Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
