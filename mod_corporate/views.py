5d79ab5e9e27053200123f0041af12b74a26acdc
JamesTKGDB <jcm.keir@gmail.com>
2020-11-15 16:36:38 +0200

    2020-11-15 JK Test

--- mod_corporate/views.py
+++ mod_corporate/views.py
@@ -2937,6 +2937,7 @@ def past_staff(request, cor):
     context = {'past_qs': past_qs, 'cor': cor, 'corp': corp,}
     return render(request, template, context)
 
+    #2020-11-15 JK Changes for test
 
 
     #count staff at each experience level
