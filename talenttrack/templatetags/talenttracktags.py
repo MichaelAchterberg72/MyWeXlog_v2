ad32d08ffde7674f395282bae8e9a794b50b65d7
MichaelAchterberg <machterberg@devoptec.com>
2021-03-15 14:22:32 +0200

    Jira MYW-376

--- talenttrack/templatetags/talenttracktags.py
+++ talenttrack/templatetags/talenttracktags.py
@@ -1,3 +1,4 @@
+import phonenumbers
 from django import template
 register = template.Library()
 
@@ -6,3 +7,8 @@ register = template.Library()
 def order_by(queryset, args):
     args = [x.strip() for x in args.split(',')]
     return queryset.order_by(*args)
+
+
+@register.filter(name='phonenumber')
+def phonenumber(value, country=None):
+   return phonenumbers.parse(value, country)
