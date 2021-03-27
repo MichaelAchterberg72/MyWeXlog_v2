11ec103428d4f206acf7a918f21fb420d6704ae1
MichaelAchterberg <machterberg@devoptec.com>
2021-03-27 11:42:50 +0200

    Jira MYW-453

--- WeXlog/settings.py
+++ WeXlog/settings.py
@@ -124,6 +124,11 @@ TEMPLATES = [
                 'users.context_processor.theme',
                 'users.context_processor.notification_count',
                 'users.context_processor.confirm_count',
+                'users.context_processor.employer_interviews_count',
+                'users.context_processor.talent_interviews_count',
+                'users.context_processor.employer_assignment_count',
+                'users.context_processor.talent_assignment_count',
+                'users.context_processor.total_notification_count',
             ],
 #            'loaders': [
 #            ('django.template.loaders.cached.Loader', [
