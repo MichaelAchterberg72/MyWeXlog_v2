fb8018519e3c6aa063cbf450686a870a21ee8aa9
Michael Achterberg <machterberg@devoptec.com>
2021-06-13 16:55:31 +0200

    Jira MYW-483

--- talenttrack/forms.py
+++ talenttrack/forms.py
@@ -137,7 +137,7 @@ class DesignationSelect2Widget(DesignationSearchFieldMixin, ModelSelect2Widget):
 
 class ProjectSearchFieldMixin:
     search_fields = [
-        'name__icontains', 'pk__startswith', 'company__ename__icontains', 'region__region__icontains', 'city__city__icontains',
+        'project__name__icontains', 'companybranch__name__icontains', 'pk__startswith', 'company__ename__icontains', 'companybranch__region__region__icontains', 'companybranch__city__city__icontains',
     ]
 
     dependent_fields = {'companybranch': 'companybranch'}
