4fce85db252dd2b1a754c5a35a16e18f686a07c4
Michael Achterberg <machterberg@devoptec.com>
2021-06-20 10:59:53 +0200

    Jira MYW-483

--- project/views.py
+++ project/views.py
@@ -280,24 +280,19 @@ def ProjectDetailView(request, prj):
 
 
 @login_required()
-#@csp_exempt
 def ProjectEditView(request, prj):
-    info2 = ProjectData.objects.get(slug=prj)
-    form = ProjectForm(request.POST or None, instance=info2)
+    instance = get_object_or_404(ProjectData, slug=prj)
+    form = ProjectForm(request.POST or None, instance=instance)
     if request.method == 'POST':
-        next_url=request.POST.get('next','/')
         if form.is_valid():
             new = form.save(commit=False)
             new.save()
-#            form.save_m2m()
-            if not next_url or not is_safe_url(url=next_url, allowed_hosts=request.get_host()):
-                next_url = reverse('Project:ProjectDetail', kwargs={'prj':prj})
-            response = HttpResponseRedirect(next_url)
-            return response
+            return redirect(reverse('Project:ProjectDetail', kwargs={'prj':prj}))
+
     else:
-        context = {'form': form}
-        template_name = 'project/project_add.html'
-        return render(request, template_name, context)
+        template = 'project/project_add.html'
+        context = {'form': form, 'instance': instance}
+        return render(request, template, context)
 
 
 @login_required()
