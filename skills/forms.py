7a29cd5bc6f2cf2fce85f20e8261a220e47c2b68
James Keir <jkeir@devoptec.com>
2021-10-29 17:06:18 +0200

    Skill Module cleanup
    Removed code in models.py and forms.py from the skills module as it was not being used.

--- skills/forms.py
+++ skills/forms.py
@@ -4,40 +4,12 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Column, Layout, Row, Submit
 from django_countries.fields import CountryField
-from django_select2.forms import ModelSelect2Widget
 
 
-from locations.models import Region
 from db_flatten.models import SkillTag
-
-from .models import SkillFilter
 
 
 #>>> Select 2
-class SkillSearchFieldMixin:
-    search_fields = [
-        'skill__icontains', 'pk__startswith'
-    ]
-
-class SkillWidget(SkillSearchFieldMixin, ModelSelect2Widget):
-    model = SkillTag
-
-    def create_value(self, value):
-        self.get_queryset().create(skill=value)
-
-class RegionSearchFieldMixin:
-    search_fields = [
-        'region__icontains', 'pk__startswith'
-    ]
-    dependent_fields = {'country': 'country'}
-
-
-class RegionWidget(RegionSearchFieldMixin, ModelSelect2Widget):
-    model = Region
-
-    def create_value(self, value):
-        self.get_queryset().create(region=value)
-
 
 #Select2 <<<
 class SkillFilterInputForm(forms.Form):
--- skills/models.py
+++ skills/models.py
@@ -1,13 +1,1 @@
 from django.db import models
-
-
-from django_countries.fields import CountryField
-
-
-class SkillFilter(models.Model):
-    country = CountryField(blank=True, blank_label='Select country (Optional)')
-    region = models.CharField('Region (Optional)', max_length=250, blank=True)
-    skill1 = models.CharField('Skill 1', max_length=150)
-    skill2 = models.CharField('Skill 2 (Optional)', max_length=150, blank=True)
-    skill3 = models.CharField('Skill 3 (Optional)', max_length=150, blank=True)
-    skill4 = models.CharField('Skill 4 (Optional)', max_length=150, blank=True)
