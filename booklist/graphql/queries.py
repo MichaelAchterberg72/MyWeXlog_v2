cf81c1d4c099ee59d072b34312301ad27df59040
Michael Achterberg <712020:79ee54b6-56ce-4254-9453-c652f2a35115@bots.bitbucket.org>
2023-04-30 13:50:18 +0200

    finish inital booklist graphql

--- booklist/graphql/queries.py
+++ booklist/graphql/queries.py
@@ -8,8 +8,22 @@ from ..models import (
     Format,
     ReadBy
 )
-from .output_types import AuthorOutputType
-from .filters import AuthorFilter
+from .output_types import (
+    AuthorOutputType,
+    PublisherOutputType,
+    GenreOutputType,
+    BookListOutputType,
+    FormatOutputType,
+    ReadByOutputType
+)
+from .filters import (
+    AuthorFilter,
+    PublisherFilter,
+    GenreFilter,
+    BookListFilter,
+    FormatFilter,
+    ReadByFilter
+)
 
 
 class Query(graphene.ObjectType):
@@ -18,9 +32,64 @@ class Query(graphene.ObjectType):
             AuthorOutputType, 
             filterset_class=AuthorFilter
         )
+    publisher = graphene.Field(PublisherOutputType, id=graphene.ID())
+    publishers = DjangoFilterConnectionField(
+            PublisherOutputType, 
+            filterset_class=PublisherFilter
+        )
+    genre = graphene.Field(GenreOutputType, id=graphene.ID())
+    genres = DjangoFilterConnectionField(
+            GenreOutputType, 
+            filterset_class=GenreFilter
+        )
+    booklist = graphene.Field(BookListOutputType, slug=graphene.String())
+    booklists = DjangoFilterConnectionField(
+            BookListOutputType, 
+            filterset_class=BookListFilter
+        )
+    format = graphene.Field(FormatOutputType, slug=graphene.String())
+    formats = DjangoFilterConnectionField(
+            FormatOutputType, 
+            filterset_class=FormatFilter
+        )
+    readby = graphene.Field(ReadByOutputType, slug=graphene.String())
+    readbys = DjangoFilterConnectionField(
+            ReadByOutputType, 
+            filterset_class=ReadByFilter
+        )
 
     def resolve_author(self, info, id):
         try:
             return Author.objects.get(pk=id)
         except Author.DoesNotExist:
+            return None
+
+    def resolve_publisher(self, info, id):
+        try:
+            return Publisher.objects.get(pk=id)
+        except Publisher.DoesNotExist:
+            return None
+
+    def resolve_genre(self, info, id):
+        try:
+            return Genre.objects.get(pk=id)
+        except Genre.DoesNotExist:
+            return None
+
+    def resolve_booklist(self, info, id):
+        try:
+            return BookList.objects.get(pk=id)
+        except BookList.DoesNotExist:
+            return None
+
+    def resolve_format(self, info, id):
+        try:
+            return Format.objects.get(pk=id)
+        except Format.DoesNotExist:
+            return None
+
+    def resolve_readby(self, info, id):
+        try:
+            return ReadBy.objects.get(pk=id)
+        except ReadBy.DoesNotExist:
             return None
